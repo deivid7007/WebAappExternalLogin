8c900ad76974796fe965c4393e0681866743d28e
Deivid Mladenov <deividmladenov@outlook.com>
2019-02-07 15:15:49 +0200

    Included external Facebook login inside the project.

---
+++ WebAppExternalLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppExternalLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
